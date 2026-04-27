1bf0bc2a4ba2893318fd5ea0fdd071ce87bd324a
Icerath <dorjegilfillan@gmail.com>
2026-04-27 14:23:02 +0200

    remove print

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,6 @@ use std::time::Instant;
 use petty_shogi::Board;
 
 fn main() {
-    println!("finished magic");
     let start = Instant::now();
     let mut board = Board::start_pos();
     println!("total: {}", board.print_perft(5));
